629349eaca8386411b0eb28b4db6d1ef96d1928a
Morgan Stanley <morganpstanley@gmail.com>
2019-07-10 22:14:11 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,17 +7,25 @@ class School
     @roster = {}
   end
 
-  def add_student(name, grade)
-		@roster[grade] ||= []
-		@roster[grade] << name
-	end
-  
+  def add_student(student, grade)
+    if @roster.has_key?(grade)
+      @roster[grade] << student
+    else
+      @roster[grade] = []
+      @roster[grade] << student
+    end
+  end
+
   def grade(grade)
     @roster[grade]
   end
 
   def sort
-    @roster.sort!
-  end
+  		sorted = {}
+  			@roster.each do |key, value|
+  			  sorted[key] = value.sort!
+  			end
+  		sorted
+  	end
 
 end
