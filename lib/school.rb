67864df8990274011b9378514531c14fb94c7556
Morgan Stanley <morganpstanley@gmail.com>
2019-07-10 22:12:22 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,23 +1,35 @@
-# code Here
+# code here!
 class School
-  attr_reader :name, :roster
 
-  def initialize(name)
-    @name = name
-    @roster = {}
-  end
+	attr_accessor :roster
+	attr_reader :name
 
-  def add_student(name, grade)
+	def initialize(name)
+		@name = name
+		@roster = {}
+	end
+
+	def add_student(name, grade)
 		@roster[grade] ||= []
 		@roster[grade] << name
 	end
-  
-  def grade(grade)
-    @roster[grade]
-  end
 
-  def sort
-    @roster.sort
-  end
+	def grade(grade)
+		@roster[grade]
+	end
 
-end
+	#def sort
+	#	@roster.each do |key, value|
+	#	  return key, value.sort!
+	#	end
+	#end
+
+	def sort
+		sorted = {}
+			@roster.each do |key, value|
+			  sorted[key] = value.sort!
+			end
+		sorted
+	end
+
+end  
